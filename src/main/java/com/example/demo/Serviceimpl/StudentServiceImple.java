020e33c95917f4dbf2b9e7b5a3c4efe0d949a4f7
bala-amypo <bala@amypo.in>
2025-12-19 06:41:37 +0000

    User commit

--- src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
+++ src/main/java/com/example/demo/Serviceimpl/StudentServiceImple.java
@@ -21,4 +21,18 @@ public class StudentServiceImple implements StudentService {
         return studentRepository.findById(id)
         .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
     }
+    public Student updatedata(Long id,Student student){
+        Student exits=getStudentById(id);
+        exits.setName(student.getName());
+        exits.setEmail(student.getEmail());
+        return studentRepository.save(exits);
+        // .orElseThrow(()-> new ResourceNotFoundException("Student not found"));
+    }
+
+    public Student deletdata(Long id){
+        Student student=getStudentById(id);
+        studentRepository.delete(student);
+        return student;
+    }
+
 }
--- src/main/java/com/example/demo/entity/Student.java
+++ src/main/java/com/example/demo/entity/Student.java
@@ -1,55 +1,27 @@
 package com.example.demo.entity;
 
 import jakarta.persistence.*;
+import lombok.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.Size;
-import jakarta.persistence.Column;
-
 @Entity
-//@Table(name = "students")
+// @getter
+// @setter
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Student {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    @NotBlank(message="Name feild cannot be empty")
+    @NotBlank(message="Name filed cannot be empty")
     @Size(min=3,max=20,message="The user name must be min of 3 and max of 20 character")
     private String name;
     @Email(message="Invalid email id")
     @Column(unique=true)
     private String email;
 
-    public Student() {
-    }
-
-    public Student(Long  id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getName() {
-            return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
+    
 }
